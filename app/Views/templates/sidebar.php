6e7fec85a9461dc4fe9a21667b5e65c5c2ac83e9
atylestari <56219136+atylestari@users.noreply.github.com>
2021-07-11 18:16:50 +0700

    Fix

--- app/Views/templates/sidebar.php
+++ app/Views/templates/sidebar.php
@@ -1,7 +1,7 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
         <li class="nav-item">
-            <a class="nav-link" href="<?= base_url('admin/index'); ?>">
+            <a class="nav-link">
                 <i class="icon-grid menu-icon"></i>
                 <span class="menu-title">Dashboard</span>
             </a>
